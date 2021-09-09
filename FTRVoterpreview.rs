870120a19fd483195c45613281f2d5f322212e78
neilfutureftr <84245603+neilfutureftr@users.noreply.github.com>
2021-09-09 16:45:29 +0200

    Create FTRVoterpreview.rs
    Still needs testing/checking constraints/actually understanding the spl proxy part/ writing tests. Will send the whole folder next time, had difficulties with github today

---
+++ FTRVoterpreview.rs
@@ -0,0 +1,232 @@
+//! A simple chat program using a ring buffer to store messages.
+
+use anchor_lang::prelude::*;
+use ftrvoterstate::{FtrState,StateModifierLoc};
+use anchor_spl::token::{self, Burn, MintTo, SetAuthority, Transfer};
+
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+#[program]
+pub mod chat {
+    use super::*;
+
+    pub fn create_user(ctx: Context<UserCreator>, bump: u8) -> Result<()> {
+
+        ctx.accounts.user.authority = *ctx.accounts.authority.key;
+        ctx.accounts.user.bump = bump;
+        ctx.accounts.user.vote = 0;
+        Ok(())
+    }
+
+    pub fn send_voter(ctx: Context<VoteSenderStruct>, msg: u8) -> Result<()> {
+        let mut user = &mut ctx.accounts.user;
+        user.vote=msg;
+        Ok(())
+    }
+
+    pub fn modify_ftr_state(ctx: Context<StateModifierStruct>, data: u64) -> ProgramResult {
+        let cpi_program = ctx.accounts.ftrstateprogramaddy.clone();
+        let cpi_accounts = StateModifierLoc {
+            ftrstataccount: ctx.accounts.ftrstate.clone(),
+        };
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+        ftrvoterstate::cpi::modify_state_loc(cpi_ctx, data)
+    }
+
+
+    pub fn proxy_transfer(ctx: Context<ProxyTransfer>, amount: u64) -> ProgramResult {
+        token::transfer(ctx.accounts.into(), amount)
+    }
+
+    pub fn proxy_mint_to(ctx: Context<ProxyMintTo>, amount: u64) -> ProgramResult {
+        token::mint_to(ctx.accounts.into(), amount)
+    }
+
+    pub fn proxy_burn(ctx: Context<ProxyBurn>, amount: u64) -> ProgramResult {
+        token::burn(ctx.accounts.into(), amount)
+    }
+
+    pub fn proxy_set_authority(
+        ctx: Context<ProxySetAuthority>,
+        authority_type: AuthorityType,
+        new_authority: Option<Pubkey>,
+    ) -> ProgramResult {
+        token::set_authority(ctx.accounts.into(), authority_type.into(), new_authority)
+    }
+
+}
+
+#[derive(Accounts)]
+#[instruction(bump: u8)]
+pub struct UserCreator<'info> {
+    #[account(
+        init,
+        seeds = [authority.key().as_ref()],
+        bump = bump,
+        payer = authority,
+        space = 320,
+    )]
+    user: Account<'info, User>,
+    #[account(signer)]
+    authority: AccountInfo<'info>,
+    system_program: AccountInfo<'info>,
+}
+
+
+
+#[derive(Accounts)]
+pub struct VoteSenderStruct<'info> {
+    #[account(
+        mut,
+        seeds = [authority.key().as_ref()],
+        bump = user.bump,
+        has_one = authority,
+    )]
+    user: Account<'info, User>,
+    #[account(signer)]
+    authority: AccountInfo<'info>,
+
+}
+
+#[account]
+pub struct User {
+
+    authority: Pubkey,
+    bump: u8,
+    vote: u8,
+}
+
+#[derive(Accounts)]
+pub struct StateModifierStruct<'info> {
+    #[account(mut, owner = ftrstateprogramaddy)]
+    pub ftrstate: Account<'info, FtrState>,
+    pub ftrstateprogramaddy: AccountInfo<'info>,
+}
+
+
+
+#[error]
+pub enum ErrorCode {
+    Unknown,
+}
+
+
+
+#[derive(AnchorSerialize, AnchorDeserialize)]
+pub enum AuthorityType {
+    /// Authority to mint new tokens
+    MintTokens,
+    /// Authority to freeze any account associated with the Mint
+    FreezeAccount,
+    /// Owner of a given token account
+    AccountOwner,
+    /// Authority to close a token account
+    CloseAccount,
+}
+
+#[derive(Accounts)]
+pub struct ProxyTransfer<'info> {
+    #[account(signer)]
+    pub authority: AccountInfo<'info>,
+    #[account(mut)]
+    pub from: AccountInfo<'info>,
+    #[account(mut)]
+    pub to: AccountInfo<'info>,
+    pub token_program: AccountInfo<'info>,
+}
+
+#[derive(Accounts)]
+pub struct ProxyMintTo<'info> {
+    #[account(signer)]
+    pub authority: AccountInfo<'info>,
+    #[account(mut)]
+    pub mint: AccountInfo<'info>,
+    #[account(mut)]
+    pub to: AccountInfo<'info>,
+    pub token_program: AccountInfo<'info>,
+}
+
+#[derive(Accounts)]
+pub struct ProxyBurn<'info> {
+    #[account(signer)]
+    pub authority: AccountInfo<'info>,
+    #[account(mut)]
+    pub mint: AccountInfo<'info>,
+    #[account(mut)]
+    pub to: AccountInfo<'info>,
+    pub token_program: AccountInfo<'info>,
+}
+
+#[derive(Accounts)]
+pub struct ProxySetAuthority<'info> {
+    #[account(signer)]
+    pub current_authority: AccountInfo<'info>,
+    #[account(mut)]
+    pub account_or_mint: AccountInfo<'info>,
+    pub token_program: AccountInfo<'info>,
+}
+
+impl<'a, 'b, 'c, 'info> From<&mut ProxyTransfer<'info>>
+    for CpiContext<'a, 'b, 'c, 'info, Transfer<'info>>
+{
+    fn from(accounts: &mut ProxyTransfer<'info>) -> CpiContext<'a, 'b, 'c, 'info, Transfer<'info>> {
+        let cpi_accounts = Transfer {
+            from: accounts.from.clone(),
+            to: accounts.to.clone(),
+            authority: accounts.authority.clone(),
+        };
+        let cpi_program = accounts.token_program.clone();
+        CpiContext::new(cpi_program, cpi_accounts)
+    }
+}
+
+impl<'a, 'b, 'c, 'info> From<&mut ProxyMintTo<'info>>
+    for CpiContext<'a, 'b, 'c, 'info, MintTo<'info>>
+{
+    fn from(accounts: &mut ProxyMintTo<'info>) -> CpiContext<'a, 'b, 'c, 'info, MintTo<'info>> {
+        let cpi_accounts = MintTo {
+            mint: accounts.mint.clone(),
+            to: accounts.to.clone(),
+            authority: accounts.authority.clone(),
+        };
+        let cpi_program = accounts.token_program.clone();
+        CpiContext::new(cpi_program, cpi_accounts)
+    }
+}
+
+impl<'a, 'b, 'c, 'info> From<&mut ProxyBurn<'info>> for CpiContext<'a, 'b, 'c, 'info, Burn<'info>> {
+    fn from(accounts: &mut ProxyBurn<'info>) -> CpiContext<'a, 'b, 'c, 'info, Burn<'info>> {
+        let cpi_accounts = Burn {
+            mint: accounts.mint.clone(),
+            to: accounts.to.clone(),
+            authority: accounts.authority.clone(),
+        };
+        let cpi_program = accounts.token_program.clone();
+        CpiContext::new(cpi_program, cpi_accounts)
+    }
+}
+
+impl<'a, 'b, 'c, 'info> From<&mut ProxySetAuthority<'info>>
+    for CpiContext<'a, 'b, 'c, 'info, SetAuthority<'info>>
+{
+    fn from(
+        accounts: &mut ProxySetAuthority<'info>,
+    ) -> CpiContext<'a, 'b, 'c, 'info, SetAuthority<'info>> {
+        let cpi_accounts = SetAuthority {
+            account_or_mint: accounts.account_or_mint.clone(),
+            current_authority: accounts.current_authority.clone(),
+        }; // TODO: Support multisig signers
+        let cpi_program = accounts.token_program.clone();
+        CpiContext::new(cpi_program, cpi_accounts)
+    }
+}
+
+impl From<AuthorityType> for spl_token::instruction::AuthorityType {
+    fn from(authority_ty: AuthorityType) -> spl_token::instruction::AuthorityType {
+        match authority_ty {
+            AuthorityType::MintTokens => spl_token::instruction::AuthorityType::MintTokens,
+            AuthorityType::FreezeAccount => spl_token::instruction::AuthorityType::FreezeAccount,
+            AuthorityType::AccountOwner => spl_token::instruction::AuthorityType::AccountOwner,
+            AuthorityType::CloseAccount => spl_token::instruction::AuthorityType::CloseAccount,
+        }
+    }
